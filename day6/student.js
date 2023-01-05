521f6fe701709c553bb5321644368e3dce913e59
Phạm Minh Triết <phamminhtriet0312@gmail.com>
2023-01-05 23:51:28 +0700

    update 5/1

--- day6/student.js
+++ day6/student.js
@@ -1,12 +1,12 @@
 function student(name, age) {
-    this.name = name;
-    this.age = age;
+    this.name = name
+    this.age = age
 }
-num_student+Number(prompt("Number of student: "))
-let students=[];
-for (i=0; i<num_student; i++) {
-    let name=prompt("Name of student $(i+1): ");
-    let age=Number(prompt("Age of student $(i+1): "));
-    students.push(new student(name, age));
+num_student= Number(prompt("Number of student: "))
+let students=[]
+for (let i=0; i<num_student; i++) {
+    let name=prompt("Name of student ${i+1}") 
+    let age=prompt("Age of student ${i+1}")
+    students.push(new student(name, age))
 }
-console.log(students);
+console.log(students)
